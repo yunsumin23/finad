603d4d7bfb1e096c467fcfa23908d6b768cc73ea
Administrator <Administrator@User -2023JNJRO>
2023-09-21 14:37:42 +0900

    김유상- 기업 가입 페이지 UI수정

--- jjj/WebContent/js/signup_company.js
+++ jjj/WebContent/js/signup_company.js
@@ -14,22 +14,28 @@ $(document).ready(function() {
 		var idvalue = $('input[name="id"]').val();
 		if (id.trim() === "") {
 			$("#result").html("아이디를 입력해주세요.");
-			$("#result").css("color", "red");
+			$("#result").css("color", "#FF4343");
 			$("#result").css("font-size", "15px");
-			$("#result").css("text-align", "center");
-			$('input[name="id"]').css('border', '2px solid red');
+			$("#result").css("text-align", "right");
+			$("#result").css("margin-left", "250px");
+			$('input[name="id"]').css('border', '5px solid #FF4343');
+			$('input[name="id"]').css('margin-bottom', '10px');
 		} else if (id.length < 6 || id.length > 16) {
 			$("#result").html("아이디는 6글자에서 16글자 사이로 입력해주세요.");
-			$("#result").css("color", "red");
+			$("#result").css("color", "#FF4343");
 			$("#result").css("font-size", "15px");
-			$("#result").css("text-align", "center");
-			$('input[name="id"]').css('border', '2px solid red');
+			$("#result").css("text-align", "right");
+			$("#result").css("margin-left", "250px");
+			$('input[name="id"]').css('border', '5px solid #FF4343');
+			$('input[name="id"]').css('margin-bottom', '10px');
 		} else if (!/^[a-zA-Z0-9]+$/.test(id)) {
 			$("#result").html("아이디는 영문 대소문자와 숫자만 입력 가능합니다.");
-			$("#result").css("color", "red");
+			$("#result").css("color", "#FF4343");
 			$("#result").css("font-size", "15px");
-			$("#result").css("text-align", "center");
-			$('input[name="id"]').css('border', '2px solid red');
+			$("#result").css("text-align", "right");
+			$("#result").css("margin-left", "250px");
+			$('input[name="id"]').css('border', '5px solid #FF4343');
+			$('input[name="id"]').css('margin-bottom', '10px');
 		} else {
 			$.ajax({
 				type : "POST",
@@ -42,16 +48,20 @@ $(document).ready(function() {
 					console.log(data);
 					if (data === "true") {
 						$("#result").html("사용가능한 아이디입니다.");
-						$("#result").css("color", "blue");
+						$("#result").css("color", "#1E6CFF");
 						$("#result").css("font-size", "15px");
-						$("#result").css("text-align", "center");
-						$('input[name="id"]').css('border', 'none');
+						$("#result").css("text-align", "right");
+						$("#result").css("margin-left", "250px");
+						$('input[name="id"]').css('border', '5px solid #1E6CFF');
+						$('input[name="id"]').css('margin-bottom', '10px');
 					} else if (data === "false") {
 						$("#result").html("중복된 아이디입니다. 다시 수정해주세요");
-						$("#result").css("color", "red");
+						$("#result").css("color", "#FF4343");
 						$("#result").css("font-size", "15px");
-						$("#result").css("text-align", "center");
-						$('input[name="id"]').css('border', '2px solid red');
+						$("#result").css("text-align", "right");
+						$("#result").css("margin-left", "250px");
+						$('input[name="id"]').css('border', '5px solid #FF4343');
+						$('input[name="id"]').css('margin-bottom', '10px');
 					}
 				}
 			});
@@ -80,36 +90,41 @@ function checkPW() {
 	var text_pw2 = document.getElementsByName('pwcheck');
 	if (pw.length < 8 || pw.length > 32) {
 		check_pw.innerHTML = '비밀번호는 8글자 이상, 32글자 이하만 이용 가능합니다.';
-		check_pw.style.color = 'red';
+		check_pw.style.color = '#FF4343';
 		check_pw.style.fontSize = '15px';
-		check_pw.style.textAlign = 'center';
-		text_pw[0].style.border = '2px solid red';
-		text_pw2[0].style.border = '2px solid red';
+		check_pw.style.textAlign = 'right';
+		text_pw[0].style.border = '5px solid #FF4343';
+		text_pw2[0].style.border = '5px solid #FF4343';
+		
 		return;
 	}
 	if (pw === pwcheck) { // 엄격하게 비교하므로 예기치 않은 형변환을 방지하고 정확한 비교를 수행합니다.
 		check_pw.innerHTML = '비밀번호가 일치합니다.';
-		check_pw.style.color = 'blue';
+		check_pw.style.color = '#1E6CFF';
 		check_pw.style.fontSize = '15px';
-		check_pw.style.textAlign = 'center';
-		text_pw[0].style.border = 'none';
-		text_pw2[0].style.border = 'none';
+		check_pw.style.textAlign = 'right';
+		text_pw[0].style.border = '5px solid #1E6CFF';
+		text_pw2[0].style.border = '5px solid #1E6CFF';
+
 	} else {
 		check_pw.innerHTML = '비밀번호가 일치하지 않습니다.';
-		check_pw.style.color = 'red';
+		check_pw.style.color = '#FF4343';
 		check_pw.style.fontSize = '15px';
-		check_pw.style.textAlign = 'center';
-		text_pw[0].style.border = '2px solid red';
-		text_pw2[0].style.border = '2px solid red';
+		check_pw.style.textAlign = 'right';
+		text_pw[0].style.border = '5px solid #FF4343';
+		text_pw2[0].style.border = '5px solid #FF4343';
+		
 		return;
 	}
 	if(pw.length < 1 && pwcheck.length < 1) {
 		check_pw.innerHTML = '비밀번호 또는 비밀번호 확인을 입력해주세요.';
-		check_pw.style.color = 'red';
+		check_pw.style.color = '#FF4343';
 		check_pw.style.fontSize = '15px';
-		check_pw.style.textAlign = 'center';
-		text_pw[0].style.border = '2px solid red';
-		text_pw2[0].style.border = '2px solid red';
+		check_pw.style.textAlign = 'right';
+		text_pw[0].style.border = '5px solid #FF4343';
+		text_pw2[0].style.border = '5px solid #FF4343';
+		
+	
 	}
 }
 function checkComName() {
@@ -118,14 +133,14 @@ function checkComName() {
 	var text_comname = document.getElementsByName('company_name');
 	if (name.length < 1) {
 		check_comname.innerHTML = '회사명을 입력해주세요.';
-		check_comname.style.color = 'red';
+		check_comname.style.color = '#FF4343';
 		check_comname.style.fontSize = '15px';
-		check_comname.style.textAlign = 'center';
-		text_comname[0].style.border = '2px solid red';
+		check_comname.style.textAlign = 'right';
+		text_comname[0].style.border = '5px solid #FF4343';
 		return;
 	} else {
 		check_comname.innerHTML = '';
-		text_comname[0].style.border = 'none';
+		text_comname[0].style.border = '5px solid #1E6CFF';
 	}
 	console.log(name);
 	console.log(check_name);
@@ -136,14 +151,14 @@ function checkName() {
 	var text_name = document.getElementsByName('company_person');
 	if (name.length < 1) {
 		check_name.innerHTML = '담당자 이름을 입력해주세요.';
-		check_name.style.color = 'red';
+		check_name.style.color = '#FF4343';
 		check_name.style.fontSize = '15px';
-		check_name.style.textAlign = 'center';
-		text_name[0].style.border = '2px solid red';
+		check_name.style.textAlign = 'right';
+		text_name[0].style.border = '5px solid #FF4343';
 		return;
 	} else {
 		check_name.innerHTML = '';
-		text_name[0].style.border = 'none';
+		text_name[0].style.border = '5px solid #1E6CFF';
 	}
 	console.log(name);
 	console.log(check_name);
@@ -154,14 +169,14 @@ function checkPhone() {
 	var text_phone = document.getElementsByName('company_phone');
 	if (phone.length < 1) {
 		check_phone.innerHTML = '담당자 전화번호를 입력해주세요.';
-		check_phone.style.color = 'red';
+		check_phone.style.color = '#FF4343';
 		check_phone.style.fontSize = '15px';
-		check_phone.style.textAlign = 'center';
-		text_phone[0].style.border = '2px solid red';
+		check_phone.style.textAlign = 'right';
+		text_phone[0].style.border = '5px solid #FF4343';
 		return;
 	} else {
 		check_phone.innerHTML = '';
-		text_phone[0].style.border = 'none';
+		text_phone[0].style.border = '5px solid #1E6CFF';
 	}
 }
 function checkEmail() {
@@ -170,14 +185,14 @@ function checkEmail() {
 	var text_email = document.getElementsByName('company_email');
 	if (email.length < 1) {
 		check_email.innerHTML = '이메일을 입력해주세요.';
-		check_email.style.color = 'red';
+		check_email.style.color = '#FF4343';
 		check_email.style.fontSize = '15px';
-		check_email.style.textAlign = 'center';
-		text_email[0].style.border = '2px solid red';
+		check_email.style.textAlign = 'right';
+		text_email[0].style.border = '5px solid #FF4343';
 		return;
 	} else {
 		check_email.innerHTML = '';
-		text_email[0].style.border = 'none';
+		text_email[0].style.border = '5px solid #1E6CFF';
 	}
 }
 function checkNumber() {
@@ -186,14 +201,14 @@ function checkNumber() {
 	var text_number = document.getElementsByName('company_number');
 	if (number.length < 1) {
 		check_number.innerHTML = '사업자/법인번호를 입력해주세요.';
-		check_number.style.color = 'red';
+		check_number.style.color = '#FF4343';
 		check_number.style.fontSize = '15px';
-		check_number.style.textAlign = 'center';
-		text_number[0].style.border = '2px solid red';
+		check_number.style.textAlign = 'right';
+		text_number[0].style.border = '5px solid #FF4343';
 		return;
 	} else {
 		check_number.innerHTML = '';
-		text_number[0].style.border = 'none';
+		text_number[0].style.border = '5px solid #1E6CFF';
 	}
 }
 function checkForm() {
