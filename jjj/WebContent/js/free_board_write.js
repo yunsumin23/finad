b451df89b1c7953815c3cdb2746b0d5da78f19c5
Administrator <Administrator@User -2023RKYYE>
2023-09-22 15:20:53 +0900

    윤수민 - 자유게시판 이제 로그인 해야 글쓰기 가능

--- jjj/WebContent/js/free_board_write.js
+++ jjj/WebContent/js/free_board_write.js
@@ -9,8 +9,16 @@ $(document).ready(function() {
         $(".gnb").stop().slideUp();
         $(".gnbbox").stop().slideUp();
     });
-    ClassicEditor.create( document.querySelector( '#editor' ), {
-        language: "ko"
+    ClassicEditor.create(document.querySelector('#editor'), {
+        // CKEditor 구성 옵션 설정
+    	
+    })
+    .then(editor => {
+        // CKEditor가 생성되면 할당된 편집기 인스턴스에 대한 작업을 수행할 수 있습니다.
+        console.log('CKEditor가 성공적으로 생성되었습니다.');
+    })
+    .catch(error => {
+        console.error('CKEditor 생성 중 오류가 발생했습니다:', error);
     });
 });
 
@@ -27,16 +35,20 @@ function zoomOut(event) {
 }
 
 function check_name() {
-    var name = document.forms["form"]["input_name"].value;
-    var text = document.forms["form"]["input_text"].value;
-
-    if(name === ""){
-    	alert("제목을 입력해주세요.");
-    	return false;
+    var name = document.getElementsByName("input_name")[0].value;
+    var text = document.getElementsByName("input_text")[0].value;
+//    var elements = document.getElementsByClassName("ck ck-reset ck-editor ck-rounded-corners");
+//    for (var i = 0; i < elements.length; i++) {
+//        // element를 사용하여 원하는 작업을 수행합니다.
+//    }
+//    alert(elements);
+    if (name === "") {
+        alert("제목을 입력해주세요.");
+        return false;
     }
-    if(text === ""){
-    	alert("내용을 입력해주세요.");
-    	return false;
+    if (text === "") {
+        alert("내용을 입력해주세요.");
+        return false;
     }
     return true;
 }
---
+++ jjj/src/com/project/jjj/FreeBoardHits.java
@@ -0,0 +1,5 @@
+package com.project.jjj;
+
+public class FreeBoardHits {
+
+}
